a9504108cc4a22de7291b656f1ba813d7211347b
Michael Ficarra <git@michael.ficarra.me>
2011-06-05 05:44:12 -0400

    efficiency enhancement: don't download the file if we already have it

--- scrape.rb
+++ scrape.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'rubygems'
 require 'uri'
@@ -9,36 +11,50 @@ output_dir = './out'
 Dir.mkdir output_dir unless File.exists? output_dir
 
 ARGV.each do |domain|
+
+	outfile = lambda do |uri|
+		"#{output_dir}/#{domain}/#{File.basename uri.path}"
+	end
+	get_response = lambda do |uri|
+		Thread.exit if File.exists? outfile.call uri
+		Net::HTTP.get_response uri
+	end
+
 	puts "Scraping #{domain}"
-	Dir.mkdir "#{output_dir}/#{domain}" unless File.exists? "#{output_dir}/#{domain}"
+	Dir.mkdir outfile.call(".") unless File.exists? outfile.call(".")
+
 	page = 1
-	begin
-		doc = Nokogiri::HTML(Net::HTTP.get(domain, "/page/#{page}"))
+    doc = Nokogiri::HTML(Net::HTTP.get(domain, "/"))
+    until doc.css("#content .pagination #next").length < 1
+        doc = Nokogiri::HTML(Net::HTTP.get(domain, "/page/#{page}")) unless page == 1
 		current_page = doc.css("#content .pagination .total")[0].content
+
 		print "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
 		print "Scraping page #{current_page} ..."
 		$stdout.flush
+
 		threads = []
 		doc.css("#content .post .post-photo a").each do |anchor|
 			href = anchor.attr "href"
 			threads << Thread.new(href) do
 				begin
 					uri = URI.parse href
-					response = Net::HTTP.get_response uri
+					response = get_response.call uri
 					while response.kind_of? Net::HTTPRedirection
 						uri = URI.parse response.header["Location"]
-						response = Net::HTTP.get_response uri
+						response = get_response.call uri
 					end
-				rescue Error
+				rescue Exception => e
+					puts e.inspect
 				else
-					outfile = "#{output_dir}/#{domain}/#{File.basename uri.path}"
-					File.open(outfile, "w"){|f| f.write response.body } unless File.exists? outfile
+					File.open(outfile.call(uri), "w"){|f| f.write response.body }
 				end
 			end
 		end
 		threads.each{|t| t.join }
-		puts
+
 		page = page + 1
-	end until doc.css("#content .pagination #next").length < 1
+	end
+
 	puts
 end
